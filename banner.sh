1deee7126cf130c74064ef9f7ebca8b61da552ac
RAFSUN-BOSS <mdrafsunemon1@gmail.com>
2026-01-28 11:18:41 +0600

    Create banner.sh for Termux welcome screen
    Add a banner script for Termux with welcome messages and instructions.

---
+++ banner.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+clear
+toilet -f big -F border "HACKER PAD"
+echo -e "\e[1;32mCreator : @rafsunboss\e[0m"
+echo
+echo -e "\e[1;34mWelcome to Termux\e[0m"
+echo
+echo "Docs:       https://doc.termux.com"
+echo "Community:  https://community.termux.com"
+echo
+echo "Working with packages:"
+echo " - Search:  pkg search <query>"
+echo " - Install: pkg install <package>"
+echo " - Upgrade: pkg upgrade"
+echo
+echo "Report issues at https://bugs.termux.com"
+echo
