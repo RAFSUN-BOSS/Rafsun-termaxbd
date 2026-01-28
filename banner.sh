11434552744edbae0e2af26eb091fb8c6430c77d
RAFSUN-BOSS <mdrafsunemon1@gmail.com>
2026-01-28 11:43:19 +0600

    Update banner function in banner.sh
    Refactor banner function to use heredoc for output.

--- banner.sh
+++ banner.sh
@@ -1,18 +1,14 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
+banner() {
 clear
-toilet -f big -F border "HACKER PAD"
-echo -e "\e[1;32mCreator : @rafsunboss\e[0m"
-echo
-echo -e "\e[1;34mWelcome to Termux\e[0m"
-echo
-echo "Docs:       https://doc.termux.com"
-echo "Community:  https://community.termux.com"
-echo
-echo "Working with packages:"
-echo " - Search:  pkg search <query>"
-echo " - Install: pkg install <package>"
-echo " - Upgrade: pkg upgrade"
-echo
-echo "Report issues at https://bugs.termux.com"
-echo
+cat <<'EOF'
+ _    _    ____ _  __ _____ ____
+| | | |  / \  / ___| |/ /| ____|  _ \
+| |_| | / _ \| |   | ' / |  _| | |_) |
+|  _  |/ ___ \ |___| . \ | |___|  _ <
+|_| |_/_/   \_\____|_|\_\|_____|_| \_\
+
+              RAFSUN-BOSS
+Author  : Rafsun Boss
+Version : 1.4
+EOF
+}
