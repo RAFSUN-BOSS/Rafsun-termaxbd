55636d938ca5f57b2fb6286ff7baf31f994d44a1
RAFSUN-BOSS <mdrafsunemon1@gmail.com>
2026-01-28 12:17:18 +0600

    Modify banner to display dynamic version from file
    Updated banner function to include dynamic versioning.

--- banner.sh
+++ banner.sh
@@ -1,14 +1,19 @@
 banner() {
 clear
-cat <<'EOF'
- _    _    ____ _  __ _____ ____
-| | | |  / \  / ___| |/ /| ____|  _ \
-| |_| | / _ \| |   | ' / |  _| | |_) |
-|  _  |/ ___ \ |___| . \ | |___|  _ <
-|_| |_/_/   \_\____|_|\_\|_____|_| \_\
 
+VERSION_FILE="$HOME/Rafsun-termaxbd/version.txt"
+VERSION="unknown"
+[ -f "$VERSION_FILE" ] && VERSION=$(cat "$VERSION_FILE")
+
+cat <<EOF
+ _   _    _    ____ _  __ _____ ____
+| | | |  / \  / ___| |/ /| ____|  _ \\
+| |_| | / _ \| |   | ' / |  _| | |_) |
+|  _  |/ ___ \\ |___| . \\ | |___|  _ <
+|_| |_/_/   \\_\\____|_|\\_\\|_____|_| \\_\\
               RAFSUN-BOSS
 Author  : Rafsun Boss
-Version : 1.4
+Version : $VERSION
+
 EOF
 }
