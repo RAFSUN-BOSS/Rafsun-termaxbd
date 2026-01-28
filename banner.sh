07e5d7fe22bdf98dca602750f141c3833633d10a
RAFSUN-BOSS <mdrafsunemon1@gmail.com>
2026-01-28 12:31:50 +0600

    Refactor banner function and simplify output
    Refactor banner function to use heredoc for ASCII art and simplify version retrieval.

--- banner.sh
+++ banner.sh
@@ -1,27 +1,17 @@
 banner() {
 
-VERSION_FILE="$HOME/Rafsun-termaxbd/version.txt"
-VERSION="unknown"
-[ -f "$VERSION_FILE" ] && VERSION=$(cat "$VERSION_FILE")
+VERSION=$(cat "$HOME/Rafsun-termaxbd/version.txt" 2>/dev/null)
 
-# -------- RGB function --------
-rgb() {
-    printf "\033[38;2;%s;%s;%sm" "$1" "$2" "$3"
-}
-
-reset="\033[0m"
-
-clear
-
-# -------- Banner --------
-echo -e "$(rgb 255 0 0) _   _    _    ____ _  __ _____ ____$(reset)"
-echo -e "$(rgb 255 80 0)| | | |  / \\  / ___| |/ /| ____|  _ \\$(reset)"
-echo -e "$(rgb 255 160 0)| |_| | / _ \\| |   | ' / |  _| | |_) |$(reset)"
-echo -e "$(rgb 0 255 120)|  _  |/ ___ \\ |___| . \\ | |___|  _ <$(reset)"
-echo -e "$(rgb 0 180 255)|_| |_/_/   \\_\\____|_|\\_\\|_____|_| \\_\\$(reset)"
-
-echo -e "$(rgb 180 0 255)              RAFSUN-BOSS$(reset)"
-echo -e "$(rgb 200 200 200)Author  : Rafsun Boss$(reset)"
-echo -e "$(rgb 200 200 200)Version : $VERSION$(reset)"
-echo
+echo -e "\033[1;36m"
+cat <<'EOF'
+ _   _    _    ____ _  __ _____ ____  
+| | | |  / \  / ___| |/ /| ____|  _ \  
+| |_| | / _ \| |   | ' / |  _| | |_) |  
+|  _  |/ ___ \ |___| . \ | |___|  _ <  
+|_| |_/_/   \_\____|_|\_\|_____|_| \_\  
+EOF
+echo -e "\033[1;35m              RAFSUN-BOSS"
+echo -e "\033[1;33mAuthor  : Rafsun Boss"
+echo -e "\033[1;32mVersion : $VERSION"
+echo -e "\033[0m"
 }
