c444af9eee48df3d3bb1ed138728d9c3fd028696
RAFSUN-BOSS <mdrafsunemon1@gmail.com>
2026-01-28 12:20:15 +0600

    Enhance banner function with RGB color support
    Updated banner function to use RGB colors for output.

--- banner.sh
+++ banner.sh
@@ -1,19 +1,27 @@
 banner() {
-clear
 
 VERSION_FILE="$HOME/Rafsun-termaxbd/version.txt"
 VERSION="unknown"
 [ -f "$VERSION_FILE" ] && VERSION=$(cat "$VERSION_FILE")
 
-cat <<EOF
- _   _    _    ____ _  __ _____ ____
-| | | |  / \  / ___| |/ /| ____|  _ \\
-| |_| | / _ \| |   | ' / |  _| | |_) |
-|  _  |/ ___ \\ |___| . \\ | |___|  _ <
-|_| |_/_/   \\_\\____|_|\\_\\|_____|_| \\_\\
-              RAFSUN-BOSS
-Author  : Rafsun Boss
-Version : $VERSION
+# -------- RGB function --------
+rgb() {
+    printf "\033[38;2;%s;%s;%sm" "$1" "$2" "$3"
+}
 
-EOF
+reset="\033[0m"
+
+clear
+
+# -------- Banner --------
+echo -e "$(rgb 255 0 0) _   _    _    ____ _  __ _____ ____$(reset)"
+echo -e "$(rgb 255 80 0)| | | |  / \\  / ___| |/ /| ____|  _ \\$(reset)"
+echo -e "$(rgb 255 160 0)| |_| | / _ \\| |   | ' / |  _| | |_) |$(reset)"
+echo -e "$(rgb 0 255 120)|  _  |/ ___ \\ |___| . \\ | |___|  _ <$(reset)"
+echo -e "$(rgb 0 180 255)|_| |_/_/   \\_\\____|_|\\_\\|_____|_| \\_\\$(reset)"
+
+echo -e "$(rgb 180 0 255)              RAFSUN-BOSS$(reset)"
+echo -e "$(rgb 200 200 200)Author  : Rafsun Boss$(reset)"
+echo -e "$(rgb 200 200 200)Version : $VERSION$(reset)"
+echo
 }
